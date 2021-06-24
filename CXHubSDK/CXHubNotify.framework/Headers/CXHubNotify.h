05f2efd2c70e1ad625a17025aeb6664811fcc5a6
Oleg Fedyakin <acid.lynx@ya.ru>
2021-06-25 02:46:36 +0300

    Release v0.0.7

---
+++ CXHubSDK/CXHubNotify.framework/Headers/CXHubNotify.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import <CXHubNotify/CXNotify.h>
+#import <CXHubNotify/CXNotifyApi.h>
+#import <CXHubNotify/CXNotifyExtensionApi.h>
+#import <CXHubNotify/CXNotifyDelegate.h>
+#import <CXHubNotify/CXContentExtensionData.h>
+#import <CXHubNotify/CXContentExtensionDelegate.h>
